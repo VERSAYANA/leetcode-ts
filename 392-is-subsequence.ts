16c85e7816bafa3a5e5354fcd6e2b49cc9b2b63f
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-04 23:21:33 +0330

    Solve 392

---
+++ 392-is-subsequence.ts
@@ -0,0 +1,14 @@
+function isSubsequence(s: string, t: string): boolean {
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j <= t.length; j++) {
+      if (s[i] === t[j]) {
+        i += 1;
+        continue;
+      }
+      if (j >= t.length) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
