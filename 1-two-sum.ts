5ddd280c0148b96c65d0fd8d960a08400dd71c30
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-05 22:13:26 +0330

    Solve 21

--- 1-two-sum.ts
+++ 1-two-sum.ts
@@ -1,22 +1,26 @@
 function twoSum(nums: number[], target: number): number[] {
-    let numsMap = new Map<number, number>();
-    numsMap.set(nums[0], 0)
-    for (let i = 1; i < nums.length; i++) {
-        if (numsMap.has(target - nums[i])) {
-            return [numsMap.get(target - nums[i]), i]
-        }
-        numsMap.set(nums[i], i)
+  let numsMap = new Map<number, number>();
+  numsMap.set(nums[0], 0);
+  for (let i = 1; i < nums.length; i++) {
+    if (numsMap.has(target - nums[i])) {
+      return [numsMap.get(target - nums[i]) as number, i];
     }
-};
+    numsMap.set(nums[i], i);
+  }
+
+  return [];
+}
 
 function twoSum2(nums: number[], target: number): number[] {
-    let numsMap = new Map<number, number>();
-    numsMap.set(nums[0], 0)
-    for (let i = 1; i < nums.length; i++) {
-        const difIndex = numsMap.get(target - nums[i])
-        if (difIndex !== undefined) {
-            return [difIndex, i]
-        }
-        numsMap.set(nums[i], i)
+  let numsMap = new Map<number, number>();
+  numsMap.set(nums[0], 0);
+  for (let i = 1; i < nums.length; i++) {
+    const difIndex = numsMap.get(target - nums[i]);
+    if (difIndex !== undefined) {
+      return [difIndex, i];
     }
-};
+    numsMap.set(nums[i], i);
+  }
+
+  return [];
+}
---
+++ 21-merge-two-sorted-lists.ts
@@ -0,0 +1,34 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (list1 === null) return list2;
+  else if (list2 === null) return list1;
+  else {
+    if (list1.val <= list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2);
+      return list1;
+    } else {
+      list2 = mergeTwoLists(list1, list2.next);
+      return list2;
+    }
+  }
+}
+const arrayToLinkedList = (arr: number[]): ListNode | null => {
+  let result = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result = new ListNode(arr[i], result);
+  }
+  return result;
+};
+
+mergeTwoLists(arrayToLinkedList([1, 2, 4]), arrayToLinkedList([1, 3, 4]));
