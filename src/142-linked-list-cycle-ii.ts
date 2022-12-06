9a82c044c55a7f971a6d94fff43fd07095d9c253
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-06 20:02:02 +0330

    Add another solution for 142

--- src/142-linked-list-cycle-ii.ts
+++ src/142-linked-list-cycle-ii.ts
@@ -13,3 +13,23 @@ function detectCycle(head: ListNode | null): ListNode | null {
 
   return null;
 }
+
+function detectCycle2(head: ListNode | null): ListNode | null {
+  let slow = head;
+  let fast = head;
+  while (slow !== null && fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      while (head !== slow && head !== null && slow !== null) {
+        head = head.next;
+        slow = slow.next;
+      }
+
+      return head;
+    }
+  }
+
+  return null;
+}
