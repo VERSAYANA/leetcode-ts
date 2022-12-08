617d945f3ca3d46367c8f1426b20860fdd53210d
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-08 18:49:11 +0330

    Add another solution for 589

--- src/589-n-ary-tree-preorder-traversal.ts
+++ src/589-n-ary-tree-preorder-traversal.ts
@@ -5,12 +5,11 @@ function preorder(root: Node | null): number[] {
   if (root === null) {
     return [];
   }
+
   function traverse(node: Node): void {
     result.push(node.val);
-    if (node.children) {
-      for (let i = 0; i < node.children.length; i++) {
-        traverse(node.children[i]);
-      }
+    for (const child of node.children) {
+      traverse(child);
     }
   }
 
@@ -26,7 +25,28 @@ function preorder2(root: Node | null): number[] {
 
   const result = [root.val];
   for (const child of root.children) {
-    result.push(...preorder(child));
+    result.push(...preorder2(child));
+  }
+
+  return result;
+}
+
+function preorder3(root: Node | null): number[] {
+  const result: number[] = [];
+  const stack: Node[] = [];
+
+  if (root === null) {
+    return [];
+  }
+
+  stack.push(root);
+
+  while (stack.length > 0) {
+    const currentNode = stack.pop()!;
+    result.push(currentNode.val);
+    for (let i = currentNode.children.length - 1; i >= 0; i--) {
+      stack.push(currentNode.children[i]);
+    }
   }
 
   return result;
