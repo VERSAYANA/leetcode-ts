2b13e3beeb7722b2e35a700abc854c8587c7bd42
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-09 23:57:37 +0330

    Add Node & TreeNode

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,26 @@ export class ListNode {
   }
 }
 
+export class Node {
+  val: number;
+  children: Node[];
+  constructor(val?: number) {
+    this.val = val === undefined ? 0 : val;
+    this.children = [];
+  }
+}
+
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 export const arrayToLinkedList = (arr: number[]): ListNode | null => {
   let result = null;
   for (let i = arr.length - 1; i >= 0; i--) {
