92a2a437396ba44a083c0bbea78d741bc2cea90c
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-13 01:46:47 +0330

    Solve 509

---
+++ src/509-fibonacci-number.ts
@@ -0,0 +1,27 @@
+function fib(n: number): number {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  let n1 = 0;
+  let n2 = 1;
+
+  for (let i = 2; i <= n; i++) {
+    const temp = n2;
+    n2 = n1 + n2;
+    n1 = temp;
+  }
+
+  return n2;
+}
+
+function fib2(n: number): number {
+  if (n === 0) return 0;
+  let n1 = 0;
+  let n2 = 1;
+
+  for (let i = 2; i <= n; i++) {
+    const temp = n1 + n2;
+    [n1, n2] = [n2, temp];
+  }
+
+  return n2;
+}
