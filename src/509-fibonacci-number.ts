032065a434b37b3d43c0d602065837bbcb7a1b1b
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-13 20:58:54 +0330

    Update 509

--- src/509-fibonacci-number.ts
+++ src/509-fibonacci-number.ts
@@ -25,3 +25,15 @@ function fib2(n: number): number {
 
   return n2;
 }
+
+function fib3(n: number): number {
+  if (n === 0) return 0;
+  let n1 = 0;
+  let n2 = 1;
+
+  for (let i = 2; i <= n; i++) {
+    [n1, n2] = [n2, n1 + n2];
+  }
+
+  return n2;
+}
